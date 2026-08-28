61c35d85a3c2cdb3c939bb75e7691a1737cc6290
Toma Barbarić <tomabarbaric1@gmail.com>
2026-08-28 06:44:55 -0700

    Add files via upload

---
+++ ray.c
@@ -0,0 +1,126 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include "ray.h"
+#include <string.h>
+
+void draw_fill_circle(SDL_Renderer *renderer, struct Circle circle, uint8_t R,uint8_t G,uint8_t B,uint8_t A)
+{
+    
+    SDL_SetRenderDrawColor(renderer,R,G,B,A);
+    double radius_squared = (circle.r * circle.r);
+    for (double x = circle.x - circle.r; x <= circle.x + circle.r; x++) {
+        for (double y = circle.y - circle.r; y <= circle.y + circle.r; y++) {
+            double distance_squared = pow(x - circle.x, 2) + pow(y - circle.y, 2);
+            if (distance_squared <= radius_squared) {
+                SDL_Rect pixel = (SDL_Rect){ (int)x, (int)y, 1, 1 };
+                if(SDL_RenderDrawPoint(renderer,(int)x,(int)y) != 0){printf("ERR><><");}
+  
+            }
+        }
+    }
+}
+
+void shoot_rays(struct Circle shooter_circle, struct ray ray_Arr[], SDL_Renderer* renderer, struct Circle circle)
+{
+    double x_start = shooter_circle.x;
+    double y_start = shooter_circle.y; 
+    double angle_arr[RAYS_MAX]; 
+    double length = 1600;   
+
+    int a = 0;
+    for (double i = 0; i < RAYS_MAX; i++)
+    {
+        double angle = (i / RAYS_MAX) * 360;
+        angle_arr[a++] = angle;
+    }
+    
+    double radian_arr[RAYS_MAX];
+    for (int i = 0; i < RAYS_MAX; i++)
+    {
+        radian_arr[i] = angle_arr[i] * (M_PI / 180.0f);
+    }
+
+    double radius_squared = circle.r * circle.r;
+
+    for (int i = 0; i < RAYS_MAX; i++)
+    {
+        double dx = cos(radian_arr[i]);   
+        double dy = sin(radian_arr[i]);
+
+        double step_size = 1.0;
+        double t = 0;
+        double stop_t = length;   
+
+        while (t < length)
+        {
+            double px = x_start + dx * t;
+            double py = y_start + dy * t;
+
+            double distance_squared = pow(px - circle.x, 2) + pow(py - circle.y, 2);
+
+            if (distance_squared <= radius_squared)
+            {
+                stop_t = t;   
+                break;
+            }
+
+            t += step_size;
+        }
+
+        ray_Arr[i].start_x = x_start;
+        ray_Arr[i].start_y = y_start;
+        ray_Arr[i].end_x = x_start + dx * stop_t;
+        ray_Arr[i].end_y = y_start + dy * stop_t;
+    }
+
+    for (int i = 0; i < RAYS_MAX; i++)
+    {
+        if (SDL_RenderDrawLineF(renderer, ray_Arr[i].start_x, ray_Arr[i].start_y, ray_Arr[i].end_x, ray_Arr[i].end_y) != 0)
+        {
+            printf("render failed");
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("raysim", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    SDL_Renderer* r = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    struct Circle krug = {100,100,70}; 
+    struct Circle shadow_krug = {800,400,170};
+    struct ray ray_arr[RAYS_MAX];
+    bool sim_running = true;
+    SDL_Event event;
+
+    while (sim_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT) {
+                sim_running = false;
+            }
+               if(event.type == SDL_MOUSEMOTION && event.motion.state != 0 )
+               {
+                krug.x = event.motion.x;
+                krug.y = event.motion.y;
+               }
+        }
+
+        SDL_SetRenderDrawColor(r,0,0,0,255);
+        SDL_RenderClear(r);
+
+        SDL_SetRenderDrawColor(r,255,255,0,255);
+        shoot_rays(krug,ray_arr,r,shadow_krug);
+
+        draw_fill_circle(r, krug,255,255,0,150); // very gpu intensive calls
+        draw_fill_circle(r, shadow_krug,150,0,0,150); //same 
+        SDL_RenderPresent(r);
+    }
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
---
+++ ray.h
@@ -0,0 +1,29 @@
+#ifndef RAY_H
+#define RAY_H
+
+struct Circle
+{
+    double x;
+    double y;
+    double r;
+};
+struct ray
+{
+    double start_x;
+    double start_y;
+    double end_x;
+    double end_y;
+};
+struct vector2
+{
+    double x;
+    double y;
+};
+
+
+#define WIDTH 1600
+#define HEIGHT 1000
+#define RAYS_MAX 200
+#define M_PI 3.14159265358979323846
+
+#endif
