49f4d9808492eb12c024d7a44e55641a315da528
Toma Barbarić <tomabarbaric1@gmail.com>
2026-09-07 14:43:21 -0700

    Update ray.c

--- ray.c
+++ ray.c
@@ -57,7 +57,6 @@ void shoot_rays(struct Circle shooter_circle, struct ray ray_Arr[], SDL_Renderer
         {
             double px = x_start + dx * t;
             double py = y_start + dy * t;
-
             double distance_squared = pow(px - circle.x, 2) + pow(py - circle.y, 2);
 
             if (distance_squared <= radius_squared)
@@ -84,16 +83,57 @@ void shoot_rays(struct Circle shooter_circle, struct ray ray_Arr[], SDL_Renderer
     }
 }
 
+int menu(SDL_Renderer *renderer)
+{
+    bool trigger = 0;
+    SDL_Event event;
+
+    while(trigger == 0)
+    {
+        SDL_Texture *t = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STATIC,20,20);
+        SDL_Surface *surface = SDL_LoadBMP("arrow_right.bmp");
+        SDL_Rect menu = {.h = MENU_HEIGHT, .w = MENU_WIDTH, .x = MENU_X, .y = MENU_Y};
+        t = SDL_CreateTextureFromSurface(renderer,surface);
+        SDL_FreeSurface(surface);
+
+        SDL_RenderCopy(renderer,t,NULL,&menu);
+        SDL_RenderPresent(renderer);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer,255,0,0,0);
+        SDL_RenderDrawRect(renderer,&menu);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer,0,0,0,0);
+        SDL_RenderClear(renderer);
+        SDL_Delay(10);
+        
+
+
+
+        while(SDL_PollEvent(&event))
+        {
+            if( (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_A) || (event.type == SDL_QUIT))
+            {
+                trigger = 1;
+            }
+        }
+    } 
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *win = SDL_CreateWindow("raysim", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Renderer* r = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *r = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     struct Circle krug = {100,100,70}; 
     struct Circle shadow_krug = {800,400,170};
     struct ray ray_arr[RAYS_MAX];
     bool sim_running = true;
+    bool menu_active = false;
     SDL_Event event;
     int32_t wheel_state = event.wheel.y;
 
@@ -101,49 +141,61 @@ int main(int argc, char *argv[])
     {
         while (SDL_PollEvent(&event))
         {
+            //
+            if( event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_Q)
+            {
+                menu(r);
+            }
+            //
+
             if (event.type == SDL_QUIT) {
                 sim_running = false;
             }
-               if(event.type == SDL_MOUSEMOTION && event.motion.state != 0 )
-               {
-                krug.x = event.motion.x;
-                krug.y = event.motion.y;
-               }
-               if(event.type == SDL_KEYDOWN)
-               {
-                    switch(event.key.keysym.scancode)
-                    {
-                        case SDL_SCANCODE_UP: shadow_krug.y -= CONTROL_SPEED_ARROWS       ;break;
-                        case SDL_SCANCODE_DOWN: shadow_krug.y += CONTROL_SPEED_ARROWS     ;break;
-                        case SDL_SCANCODE_LEFT: shadow_krug.x -= CONTROL_SPEED_ARROWS     ;break;
-                        case SDL_SCANCODE_RIGHT: shadow_krug.x += CONTROL_SPEED_ARROWS     ;break;
-                    }
-               }
-               if(event.wheel.type == SDL_MOUSEWHEEL)
-               {
-                if(event.wheel.y > 0)
+
+            if(event.type == SDL_MOUSEMOTION && event.motion.state != 0 )
+            {
+            krug.x = event.motion.x;
+            krug.y = event.motion.y;
+            }
+
+            if(event.type == SDL_KEYDOWN)
+            {
+                switch(event.key.keysym.scancode)
                 {
+                    case SDL_SCANCODE_UP: shadow_krug.y -= CONTROL_SPEED_ARROWS       ;break;
+                    case SDL_SCANCODE_DOWN: shadow_krug.y += CONTROL_SPEED_ARROWS     ;break;
+                    case SDL_SCANCODE_LEFT: shadow_krug.x -= CONTROL_SPEED_ARROWS     ;break;
+                    case SDL_SCANCODE_RIGHT: shadow_krug.x += CONTROL_SPEED_ARROWS     ;break;
+                }
+            }
+
+            if(event.wheel.type == SDL_MOUSEWHEEL)
+            {
+                if(event.wheel.y > 0){
                     shadow_krug.r += CONTROL_SPEED_WHEEL;
                 }
-
-                 if(event.wheel.y < 0)
-                {
+                 if(event.wheel.y < 0){
                     shadow_krug.r -= CONTROL_SPEED_WHEEL;
                 }
-               }
-               printf("%d",&wheel_state);
+
+            }
+
+               //if(menu_active == true)
+               //{
+                //sim_running = false;
+                //draw_menu()
+               //}
         }
 
         SDL_SetRenderDrawColor(r,0,0,0,255);
         SDL_RenderClear(r);
 
-        SDL_SetRenderDrawColor(r,255,255,0,255);
+        SDL_SetRenderDrawColor(r,255,255,0,255); //ray color
         shoot_rays(krug,ray_arr,r,shadow_krug);
+        draw_fill_circle(r, krug,R,G,B,A); // very gpu intensive calls
 
-        draw_fill_circle(r, krug,255,255,0,150); // very gpu intensive calls
-        draw_fill_circle(r, shadow_krug,150,0,0,150); //same 
-
-        //(int32_t)
+        draw_fill_circle(r, shadow_krug,255,0,0,150); //same 
+        //
         SDL_RenderPresent(r);
     }
     SDL_DestroyWindow(win);
