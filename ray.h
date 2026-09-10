6200b8056dbbee1c43befd7621ace0fc7373ee9e
Toma Barbarić <tomabarbaric1@gmail.com>
2026-09-10 08:36:05 -0700

    Modify ray.h to include color and menu dimensions
    Added RGBA color variables and updated RAYS_MAX definition.

--- ray.h
+++ ray.h
@@ -21,11 +21,18 @@ struct vector2
 };
 
 
+uint8_t R,G,B,A;
+uint32_t RAYS_MAX  = 5;
+#define RAYS_HARD_LIMIT 1000
+
 #define WIDTH 1600
 #define HEIGHT 1000
-#define RAYS_MAX 200
 #define M_PI 3.14159265358979323846
 #define CONTROL_SPEED_ARROWS 10
 #define CONTROL_SPEED_WHEEL 2
+#define MENU_X ((WIDTH/2) - (MENU_WIDTH / 2))
+#define MENU_Y ((HEIGHT/2) - (MENU_HEIGHT / 2))
+#define MENU_HEIGHT 600
+#define MENU_WIDTH 800
 
 #endif
