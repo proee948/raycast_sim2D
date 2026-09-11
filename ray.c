b2a01f24762a5bb42364e0af1f3f5c41dbbe79aa
Toma Barbarić <tomabarbaric1@gmail.com>
2026-09-11 12:49:38 -0700

    ray lenght menu addition started

--- ray.c
+++ ray.c
@@ -15,7 +15,6 @@ void draw_fill_circle(SDL_Renderer *renderer, struct Circle circle, uint8_t R,ui
         for (double y = circle.y - circle.r; y <= circle.y + circle.r; y++) {
             double distance_squared = pow(x - circle.x, 2) + pow(y - circle.y, 2);
             if (distance_squared <= radius_squared) {
-                SDL_Rect pixel = (SDL_Rect){ (int)x, (int)y, 1, 1 };
                 if(SDL_RenderDrawPoint(renderer,(int)x,(int)y) != 0){printf("ERR><><");}
   
             }
@@ -75,7 +74,7 @@ void shoot_rays(struct Circle shooter_circle, struct ray ray_Arr[], SDL_Renderer
         ray_Arr[i].end_y = y_start + dy * stop_t;
     }
 
-    for (int i = 0; i < RAYS_MAX; i++)
+    for (uint32_t i = 0; i < RAYS_MAX; i++)
     {
         if (SDL_RenderDrawLineF(renderer, ray_Arr[i].start_x, ray_Arr[i].start_y, ray_Arr[i].end_x, ray_Arr[i].end_y) != 0)
         {
@@ -95,19 +94,23 @@ char* its(void)
 int menu(SDL_Renderer *renderer)
 {
     SDL_Event event;
-    SDL_Texture *t,*ft,*fft,*ffft;
+    SDL_Texture *t1,*t2,*t3,*t4,*t5;
     SDL_Surface *surface = SDL_LoadBMP("arrow_right.bmp");
 
-    //holy magic numbers (stfu retard go ropemaxx)
+    //holy magic numbers, this is horrific but it works 
     SDL_Rect menu = {.h = MENU_HEIGHT, .w = MENU_WIDTH, .x = MENU_X, .y = MENU_Y};
     SDL_Rect submenu1 = {.h = (MENU_HEIGHT / 12), .w = (MENU_WIDTH / 12), .x = (MENU_X + (MENU_WIDTH / 1.1)), .y = (MENU_Y + 10)};
     SDL_Rect submenu4 = {.h = (MENU_HEIGHT / 8), .w = (MENU_WIDTH / 3), .x = (MENU_X + 1), .y = (MENU_Y + 1)};
     SDL_Rect submenu3 = {.h = (MENU_HEIGHT / 12), .w = (MENU_WIDTH / 12), .x = ( (submenu4.x + submenu4.w) + (10)), .y = (MENU_Y + 10)};
     SDL_Rect submenu2 = {.h = (MENU_HEIGHT / 8), .w = (MENU_WIDTH / 6), .x = submenu3.x + (submenu3.w  * 3), .y = (MENU_Y + 1)};
 
+    SDL_Rect submenu5 = {.h = (MENU_HEIGHT / 12), .w = (MENU_WIDTH / 12), .x = submenu1.x ,.y = (submenu1.y + submenu1.h) + Y_MENU_SECTION_RAZMAK};
+    SDL_Rect submenu6 = {.h = (MENU_HEIGHT / 8), .w = (MENU_WIDTH / 6), .x = submenu2.x ,.y = (submenu2.y + submenu2.h)};
+    SDL_Rect submenu7 = {.h = (MENU_HEIGHT / 12), .w = (MENU_WIDTH / 12), .x = submenu3.x ,.y = (submenu3.y + submenu3.h) + Y_MENU_SECTION_RAZMAK};
+    SDL_Rect submenu8 = {.h = (MENU_HEIGHT / 8), .w = (MENU_WIDTH / 3), .x = submenu4.x ,.y = (submenu4.y + submenu4.h)};
+
     bool trigger = 0;
-    int c = 0; 
-    t = SDL_CreateTextureFromSurface(renderer,surface);
+    t1 = SDL_CreateTextureFromSurface(renderer,surface);
     SDL_FreeSurface(surface);
 
     TTF_Init();
@@ -115,16 +118,15 @@ int menu(SDL_Renderer *renderer)
 
     SDL_Color txt_col = {.r = 0,.g = 255,.b = 0,.a = 255};
     surface =  TTF_RenderUTF8_Solid(font,"RAYS",txt_col);
-    ft = SDL_CreateTextureFromSurface(renderer,surface);
+    t2 = SDL_CreateTextureFromSurface(renderer,surface);
     SDL_FreeSurface(surface);
 
     surface = SDL_LoadBMP("arrow_left.bmp");
-    fft = SDL_CreateTextureFromSurface(renderer,surface);
+    t3 = SDL_CreateTextureFromSurface(renderer,surface);
     SDL_FreeSurface(surface);
 
-    char *s = its();
-    surface = TTF_RenderUTF8_Solid(font,s,txt_col);
-    ffft = SDL_CreateTextureFromSurface(renderer,surface);
+    surface =  TTF_RenderUTF8_Solid(font,"RAY LENGHT",txt_col);
+    t5 = SDL_CreateTextureFromSurface(renderer,surface);
     SDL_FreeSurface(surface);
 
     SDL_SetRenderDrawColor(renderer,0,0,0,0);
@@ -132,35 +134,33 @@ int menu(SDL_Renderer *renderer)
 
     while(trigger == 0)
     {
-        //if(c < 1)
-        //{
-
         char *s = its();
         surface = TTF_RenderUTF8_Solid(font,s,txt_col);
-        ffft = SDL_CreateTextureFromSurface(renderer,surface);
+        t4 = SDL_CreateTextureFromSurface(renderer,surface);
         SDL_FreeSurface(surface);
-
 
         SDL_SetRenderDrawColor(renderer,255,0,0,0);
         SDL_RenderDrawRect(renderer,&menu);
-        SDL_SetRenderDrawColor(renderer,255,0,0,0);
+        SDL_SetRenderDrawColor(renderer,0,0,0,0);
 
         SDL_RenderDrawRect(renderer,&submenu1);
         SDL_RenderDrawRect(renderer,&submenu4);
         SDL_RenderDrawRect(renderer,&submenu3);
         SDL_RenderDrawRect(renderer,&submenu2);
 
-        SDL_RenderCopy(renderer,t,NULL,&submenu1);  // draw "arrow _right"
-        SDL_RenderCopy(renderer,ft,NULL,&submenu4); //draw "RAYS"
-        SDL_RenderCopy(renderer,fft,NULL,&submenu3);
-        SDL_RenderCopy(renderer,ffft,NULL,&submenu2);
+        SDL_RenderCopy(renderer,t1,NULL,&submenu1);  // draw "arrow _right"
+        SDL_RenderCopy(renderer,t2,NULL,&submenu4); //draw "RAYS"
+        SDL_RenderCopy(renderer,t3,NULL,&submenu3);
+        SDL_RenderCopy(renderer,t4,NULL,&submenu2);
+        SDL_RenderCopy(renderer,t1,NULL,&submenu5);
+        SDL_RenderCopy(renderer,t4,NULL,&submenu6);
+        SDL_RenderCopy(renderer,t3,NULL,&submenu7);
+        SDL_RenderCopy(renderer,t5,NULL,&submenu8);
 
         SDL_RenderPresent(renderer);
 
         SDL_SetRenderDrawColor(renderer,0,0,0,0);
         SDL_RenderClear(renderer);
-        //c++; //incrementmog
-        //}
 
         while(SDL_PollEvent(&event))
         {
@@ -191,9 +191,7 @@ int main(int argc, char *argv[])
     struct Circle shadow_krug = {800,400,170};
     struct ray ray_arr[RAYS_HARD_LIMIT];
     bool sim_running = true;
-    bool menu_active = false; //???
     SDL_Event event;
-    int32_t wheel_state = event.wheel.y;
 
     while (sim_running)
     {
